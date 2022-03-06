bc92101e40a2556bf5d495cbe9bea53b0481d090
vinuppriya balachandran <vinuppriya.b@gmail.com>
2022-03-06 23:12:25 +0530

    final

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express =require("express");
-const mongoose=require("mongoose");
-const app=express();
-const dotenv=require("dotenv");
-const cors =require("cors");
-const authRoute =require("./routes/auth.js");
-const userRoute =require("./routes/user.js");
-const productRoute =require("./routes/product.js");
-const orderRoute =require("./routes/order.js");
-const cartRoute =require("./routes/carts.js");
-const stripeRoute =require("./routes/stripe.js");
-
-
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/user.js");
+const productRoute = require("./routes/product.js");
+const orderRoute = require("./routes/order.js");
+const cartRoute = require("./routes/carts.js");
+const stripeRoute = require("./routes/stripe.js");
+const bodyparser = require("body-parser");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("db connected"))
-.catch((err)=>{
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connected"))
+  .catch((err) => {
     console.log(err);
+  });
+
+app.get("/", () => {
+  console.log("test success");
 });
 
-app.get("/",()=>{
-    console.log("test success");
-})
+app.use("/api/auth", authRoute);
 
-
-
-app.use("/api/auth",authRoute)
-
-app.use("/api/user",userRoute)
-app.use("/api/product",productRoute)
-app.use("/api/order",orderRoute)
-app.use("/api/cart",cartRoute)
-app.use("/api/checkout",stripeRoute)
+app.use("/api/user", userRoute);
+app.use("/api/product", productRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/checkout", stripeRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("the server is started in", PORT));
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,39 +1,86 @@
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(
-  "sk_test_51KSOH5SJ3Qu163yRZuWvcgHCw2YmV9TTiGVgjGcGRl362SIWIRLKIiS9sWEBxo86XMYqn8qsCQnaCgvoNaTsN5Ob00XwPqXHUP"
+  "sk_test_51KZueYSH2RYxFIuZZPXJ74UEaTgrF9ZCxbcHG0oPldbVygGWT9CowE7ZVlbDEt9O2hzp7FiXgfQmz9t4ER8IBbMD00aU0SFNpU"
 );
 
-router.post("/payment", (req, res) => {
-  console.log(req.body);
+router.post("/payment", async (req, res) => {
+  // console.log(req.body);
   const { token, amount } = req.body;
   const idempontencyKey = uuidv4();
-  try {
-    stripe.customers
-      .create({
-        email: token.email,
-        source: token.id,
-      })
-      .then(async (customer) => {
-        stripe.charges.create(
-          {
-            amount: amount,
-            currency: "usd",
-            customer: customer.id,
-            shipping: {
-              name: token.card.name,
-              address: {
-                country: token.card.address_country,
-              },
-            },
-          },
-          { idempontencyKey }
-        );
-      })
-      .then((result) => res.status(200).json(result))
-      .catch((e) => console.log(e));
-  } catch (e) {
-    console.log(e);
-  }
+
+  stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then(async (customer) => {
+      stripe.charges.create({
+        amount: amount,
+        currency: "INR",
+        customer: customer.id,
+      });
+    })
+    .then((result) => res.send(result))
+    .catch((e) => console.log(e));
 });
 module.exports = router;
+
+// console.log(req.body);
+// const { token, amount } = req.body;
+// const idempontencyKey = uuidv4();
+// try {
+//   stripe.customers
+//     .create({
+//       email: token.email,
+//       source: token.id,
+//     })
+//     .then(async (customer) => {
+//       stripe.charges.create(
+//         {
+//           amount: amount,
+//           currency: "usd",
+//           customer: customer.id,
+//           shipping: {
+//             name: token.card.name,
+//             address: {
+//               country: token.card.address_country,
+//             },
+//           },
+//         },
+//         { idempontencyKey }
+//       );
+//     })
+//     .then((result) => res.status(200).json(result))
+//     .catch((e) => console.log(e));
+// } catch (e) {
+//   console.log(e);
+// }
+
+// stripe.customers
+//   .create({
+//     email: "ttestt@gmail.com",
+//     source: req.body.token.id,
+//     name: "Gautam Sharma",
+//     address: {
+//       line1: "510 Townsend St",
+//       postal_code: "98140",
+//       city: "San Francisco",
+//       state: "CA",
+//       country: "US",
+//     },
+//   })
+//   .then((customer) => {
+//     return stripe.charges.create({
+//       amount: 7000, // Charing Rs 25
+//       description: "Web Development Product",
+//       currency: "usd",
+//       customer: customer.id,
+//     });
+//   })
+//   .then((charge) => {
+//     res.status(200).json({ msg: "Success" }); // If no error occurs
+//   })
+//   .catch((err) => {
+//     res.status(500).json(err); // If some error occurs
+//   });
